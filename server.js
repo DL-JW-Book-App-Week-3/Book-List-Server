15533177bb80d2d3f784267a8d8cdb15e7c2688c
daniellogerstedt <daniel.logerstedt@gmail.com>
2017-12-11 17:02:26 -0800

    built initial scaffold for server file

---
+++ server.js
@@ -0,0 +1,7 @@
+// Server Scripts Go Below
+
+const express = required('express')
+const cors = required('cors') // Cors helps to resolve cross source issues
+const pg = required('pg')
+
+const app = express();
