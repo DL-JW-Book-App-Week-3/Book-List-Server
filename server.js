801c4a29e51fb9a9ea8e59f0073f2ad4e7479f37
daniellogerstedt <daniel.logerstedt@gmail.com>
2017-12-11 17:21:33 -0800

    finished building initial server file

--- server.js
+++ server.js
@@ -6,3 +6,19 @@ const cors = required('cors') // Cors helps to resolve cross source issues
 const pg = required('pg')
 
 const app = express();
+const PORT = process.env.PORT;
+const CLIENT_URL = process.env.CLIENT_URL;
+
+const client = new pg.Client(process.env.DATABASE_URL);
+client.connect();
+client.on('error', err => console.error(err));
+
+app.use(cors());
+
+app.get('/', (req, res) => res.send('Testing 1, 2, 3'));
+
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
+
+//export PORT=3000
+//export CLIENT_URL=http://localhost:8080
+//export DATABASE_URL=postgres://localhost:5432/task_app
